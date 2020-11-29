183ceb26fd777957caf9a7164efc1abb262684ee
soudegesu <soudegesu@hotmail.co.jp>
2020-11-29 16:57:37 +0900

    Create RecoilRoot for multiple element

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, Grid } from '@material-ui/core';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import './App.css';
 import TodoList from './components/todo/TodoList';
 
 const App: React.FC = () => {
+
+  const numOfTodoList = 4;
+
   return (
     <RecoilRoot>
       <Container>
         <Box marginTop={10}>
           <Box>Recoil Sample</Box>
-          <TodoList />
+            <Grid container spacing={2}>
+            { [...Array(numOfTodoList)].map((_, i) => <RecoilRoot key={i}><Grid ><TodoList/></Grid></RecoilRoot>)}
+            </Grid>
         </Box>
       </Container>
     </RecoilRoot>
